ad2339f699b7f47871d8e0ea67f23face77d8f91
Sudeep <dev.sudee@gmail.com>
2023-11-07 14:12:02 +0000

    .env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+require('dotenv').config();
 
 // console.log("Hello World");
 
@@ -11,5 +12,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+console.log("MESSAGE_STYLE:", process.env.MESSAGE_STYLE);
+
 module.exports = app;
